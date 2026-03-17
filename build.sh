0a63409bc873e1da7f607f7bd53bb03414851687
voranirav42 <voranirav42@gmail.com>
2026-03-17 13:02:57 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 # exit on error
 set -o errexit
 
+# ૧. પાયથોન લાઈબ્રેરી ઇન્સ્ટોલ કરો
 pip install -r requirements.txt
-playwright install chromium
+
+# ૨. Playwright અને તેના માટે જરૂરી સિસ્ટમ ફાઈલો ઇન્સ્ટોલ કરો
+playwright install --with-deps chromium
